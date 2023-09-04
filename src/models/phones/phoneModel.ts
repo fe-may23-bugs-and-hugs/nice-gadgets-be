d15567f31be79da47d732fdd2c6f4e48d32f77cc
maxyma-sanx <maxymasan.dev@gmail.com>
2023-09-04 16:39:35 +0300

    add new routes

---
+++ src/models/phones/phoneModel.ts
@@ -0,0 +1,33 @@
+import { Schema, model } from 'mongoose';
+
+const phoneSchema = new Schema({
+  _id: { type: String, required: true },
+  namespaceId: { type: String, required: true },
+  name: { type: String, required: true },
+  capacityAvailable: { type: [String], required: true },
+  capacity: { type: String, required: true },
+  priceRegular: { type: Number, required: true },
+  priceDiscount: { type: Number, required: true },
+  colorsAvailable: { type: [String], required: true },
+  color: { type: String, required: true },
+  images: { type: [String], required: true },
+  description: [
+    {
+      title: { type: String, required: true },
+      text: { type: [String], required: true },
+    },
+  ],
+  screen: { type: String, required: true },
+  resolution: { type: String, required: true },
+  processor: { type: String, required: true },
+  ram: { type: String, required: true },
+  camera: { type: String, required: true },
+  zoom: { type: String, required: true },
+  cell: { type: [String], required: true },
+  category: { type: String, required: true },
+  year: { type: Number, required: true },
+});
+
+const PhoneModel = model('phone', phoneSchema);
+
+export default PhoneModel;
